defa1ad00d92eb23e34bcf967ba3a2faf68ebb59
dakidamachine <78439292+dakidamachine@users.noreply.github.com>
2021-03-03 11:06:03 -0500

    app.getchanges

--- site1/server.js
+++ site1/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 app.use(express.static('public'));
 
-app.get('/Doraemon.html', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/Doraemon.html'));
 });
 
 app.listen(80, function () {
