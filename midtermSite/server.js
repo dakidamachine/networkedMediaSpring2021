d57277310ab10bf2b4315947602950131a7e8d8d
dakidamachine <78439292+dakidamachine@users.noreply.github.com>
2021-03-09 19:58:00 -0500

    First Commit

--- midtermSite/server.js
+++ midtermSite/server.js
@@ -1,25 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var path = require('path');
+app.use(express.static('public'));
 
-var urlencodedBodyParser = bodyParser.urlencoded({extended:true});
-
-app.use (express.static('public'));
-app.set('view engine', 'ejs');
-
-var submittedData = [];
-
-//How to set this to a specific HTML page?
-app.get('/', function(req,res){
-    res.send('Hello World');
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/Doraemon.html'));
 });
 
-app.post('/formdata', function(req,res){
-    console.log(req.body.data);
-})
-
-
-//Port
-app.listen(80,function (){
-    console.log('Example app listening on port 80')
-})
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
+});
